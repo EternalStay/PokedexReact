7a25d2941d07d8a774649fa7e48619367bc071de
EternalStay <volff.lorris@gmail.com>
2022-07-17 00:53:21 +0200

    Responsive 2

--- src/Body/Pokemon/Pokemon.js
+++ src/Body/Pokemon/Pokemon.js
@@ -21,7 +21,7 @@ export function Pokemon(props) {
     }
 
     return ( 
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center">
+        <div className="col-lg-2 col-md-3 col-sm-4 text-center">
             <div className="border m-1" style={style}>
                 <div><img className="w-50" loading="lazy" alt={p.names.fr} title={p.names.fr} src={p.sprites.front_default ?? 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/0.png'} /></div>
                 <div>{languageSelected in p.names ? p.names[languageSelected] : '???'}</div>
